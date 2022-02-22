7a7ed5ba0fa833cd9931b07cf17037be54db675e
Mikhail Chuparnov <mc_gorba4e@mail.ru>
2022-02-22 23:30:32 +0200

    added FeedViewController PostViewController ProfileViewController

---
+++ Netology-1_3-navigation/Netology-1_3-navigation/PostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewController.swift
+//  Netology-1_3-navigation
+//
+//  Created by Mikhail Chuparnov on 22.02.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+}
